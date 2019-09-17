8681dda6fbf80fd725dccd9f68ee296fe3949dd0
Juho <juho.turkki@student.saimia.fi>
2019-09-17 11:46:13 +0300

    IFteht1
    Negatives not working on mac

---
+++ IFteht1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IFteht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+
+            luku = Convert.ToInt32(Console.ReadLine());
+
+            if (luku == 0)
+            {
+                Console.WriteLine("Luku on nolla");
+            }
+            else
+            {
+                if (luku < 0)
+                {
+                    Console.WriteLine("Luku on postitiivinen");
+                }
+                else
+                {
+                    Console.WriteLine("Luku on negatiivinen");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}

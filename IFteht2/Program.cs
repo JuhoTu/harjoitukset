8d3dbf4c6cb59a74f3cbac37b3507ce07556f0d9
Juho <juho.turkki@student.saimia.fi>
2019-09-18 14:29:29 +0300

    IFteht2
    Added if section 2

---
+++ IFteht2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IFteht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

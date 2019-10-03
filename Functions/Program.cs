fa253bc0e4c1dba95f6c660ba2e532f693351d40
Juho <juho.turkki@student.saimia.fi>
2019-10-03 11:14:21 +0300

    update task1

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -6,7 +6,20 @@ namespace Functions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Syötä kuinka monta tähteä haluat tulostaa: ");
+            string input = Console.ReadLine();
+            Star(input);
+        }
+
+        static string Star (string input)
+        {
+            string output = "";
+            int n = int.Parse(input);
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"{output}*");
+            }
+            return output;
         }
     }
 }

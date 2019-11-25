18e5bb66cda35de8993119c781630ab5ca31444c
Juho <juho.turkki@student.saimia.fi>
2019-11-25 14:15:49 +0200

    updates
    very early before alpha at least some text

--- ArrayLotto/Program.cs
+++ ArrayLotto/Program.cs
@@ -6,16 +6,7 @@ namespace ArrayLotto
     {
         static void Main(string[] args)
         {
-            int[] lottoNumbers = new int[8];
-            Random rnd = new Random();
-            for (int i = 1; i <= 7; i++)
-            {
-                lottoNumbers[i] = rnd.Next(1, 40);
-            }
-            for (int i = 1; i <= 7; i++)
-            {
-                Console.Write($"{lottoNumbers[i]} ");
-            }
+            int[] lottoNumbers = new int [];
         }
     }
 }

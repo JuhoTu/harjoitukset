834ffaf9eded16c24487a4e46f1d907a63d5d53f
Juho <juho.turkki@student.saimia.fi>
2019-12-11 20:38:10 +0200

    Created task

---
+++ Array3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Array3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for()
+        }
+    }
+}

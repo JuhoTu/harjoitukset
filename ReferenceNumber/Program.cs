36c499d56d4fc7a91f7737aee5f8a5e7cc86c793
Juho <juho.turkki@student.saimia.fi>
2020-01-03 13:01:38 +0200

    created program
    created and added a simple menu

---
+++ ReferenceNumber/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ReferenceNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Checks inputted reference number or creates new ones
+            char userChoise;
+            do
+            {
+                Console.Clear();
+                userChoise = UserInterface();
+                switch (userChoise)
+                {
+                    // Check input
+                    case 'C':
+                        RefChecker();
+                        break;
+                    // Create a new one
+                    case 'N':
+                        RefCreator();
+                        break;
+                    // Create multiple new ones
+                    case 'M':
+                        RefMultiCreator();
+                        break;
+                    // Exit
+                    case 'X':
+                        break;
+
+                    default:
+                        Console.WriteLine("\nCheck input. Proceed by pressing Enter.");
+                        Console.ReadLine();
+                        break;
+                }
+                Console.ReadLine();
+            } while (userChoise != 'X');
+        }
+
+        static char UserInterface()
+        {
+            Console.WriteLine("Reference number validator and creator.");
+            Console.WriteLine("[C] Validate reference number.");
+            Console.WriteLine("[N] Create a new one.");
+            Console.WriteLine("[M] Create a new one.");
+            Console.WriteLine("[X] Close the program.");
+            Console.Write("Choose procedure: ");
+
+            return char.ToUpper(Console.ReadKey().KeyChar);
+        }
+
+        static void RefChecker()
+        {
+
+        }
+
+        static void RefCreator()
+        {
+
+        }
+
+        static void RefMultiCreator()
+        {
+
+        }
+    }
+}
